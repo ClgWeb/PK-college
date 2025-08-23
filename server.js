6e3073971c14c8ecee6b3e54d1c87c3c0f27ec33
PremnathSundarrajan <premsundarrajan4@gmail.com>
2025-08-23 18:53:00 +0530

    Login page created

--- server.js
+++ server.js
@@ -4,10 +4,15 @@ const app = express();
 app.set("view engine","ejs");
 
 app.use(express.static("public"));
-
+app.use(express.json());
 app.get('/',(req,res)=>{
     res.render("index");
 
+})
+
+app.get('/login',(req,res)=>{
+    console.log("login page opened");
+    res.render("login");
 })
 
 app.listen(PORT,()=>{

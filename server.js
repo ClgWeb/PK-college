866126665c4f5ec346619e3cb59278f4d258d63a
PremnathSundarrajan <premsundarrajan4@gmail.com>
2025-08-23 10:07:06 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const PORT= 3000;
+const app = express();
+app.set("view engine","ejs");
+
+app.use(express.static("public"));
+
+app.get('/',(req,res)=>{
+    res.render("index");
+
+})
+
+app.listen(PORT,()=>{
+    console.log(`server is running on port 3000`);
+
+})
